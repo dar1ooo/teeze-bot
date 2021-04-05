9561b43f0e74d928fb8b312e9b44b7cad3d24bac
dario.schaffner <darioschaffner7@gmail.com>
2021-04-05 18:29:36 +0200

    Added Necessarry Classes and basic creating task ( missing task list )

---
+++ teeze-bot/classes/TaskInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace teeze_bot.classes
+{
+    public class TaskInfo
+    {
+        public int TaskId;
+        public string Store;
+        public double ShoeSize;
+        public string Product;
+        public string Profile;
+        public string Proxy;
+        public string Account;
+
+        public TaskInfo(int taskId, string store, double shoeSize, string product, string profile, string proxy, string account)
+        {
+            TaskId = taskId;
+            Store = store;
+            ShoeSize = shoeSize;
+            Product = product;
+            Profile = profile;
+            Proxy = proxy;
+            Account = account;
+        }
+    }
+}
