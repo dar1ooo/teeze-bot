0db25738322a7312cf40a7e9a8ed6129fedd11ed
dario.schaffner <darioschaffner7@gmail.com>
2021-04-29 00:49:39 +0200

    added DeleteAccounts and Profiles option

---
+++ teeze-bot/classes/enums/confirm.cs
@@ -0,0 +1,13 @@
+﻿namespace teeze_bot.classes.enums
+{
+    public enum Confirm
+    {
+        Nothing = 0,
+        DeleteAllTasks = 1,
+        DeleteSpecificTask = 2,
+        DeleteAllProfiles = 3,
+        DeleteSpecificProfile = 4,
+        DeleteAllAccounts = 5,
+        DeleteSpecificAccount = 6,
+    }
+}
